2f87e327735aa5848e907a845915429e2b8a0d5a
Balaji ARAM VALARTHA SUNDARAM <balaji@lakshmibalajio2o.com>
2025-07-23 12:56:02 +0000

    map is not updated as you mentioned, to dark teal color

--- src/components/network-map.tsx
+++ src/components/network-map.tsx
@@ -4,33 +4,31 @@
 import * as React from 'react';
 import { APIProvider, Map, useMap } from '@vis.gl/react-google-maps';
 
+const mapStyles: google.maps.MapTypeStyle[] = [
+    { elementType: "geometry", stylers: [{ color: "#1d2c4d" }] },
+    { elementType: "labels.text.fill", stylers: [{ color: "#8ec3b9" }] },
+    { elementType: "labels.text.stroke", stylers: [{ color: "#1a3646" }] },
+    { featureType: "administrative.country", elementType: "geometry.stroke", stylers: [{ color: "#4b6878" }] },
+    { featureType: "administrative.land_parcel", stylers: [{ visibility: "off" }] },
+    { featureType: "administrative.province", elementType: "geometry.stroke", stylers: [{ color: "#4b6878" }] },
+    { featureType: "landscape.man_made", elementType: "geometry.stroke", stylers: [{ color: "#334e87" }] },
+    { featureType: "landscape.natural", elementType: "geometry", stylers: [{ color: "#023e58" }] },
+    { featureType: "poi", elementType: "geometry", stylers: [{ color: "#283d6a" }] },
+    { featureType: "poi", elementType: "labels.text.fill", stylers: [{ color: "#6f9ba5" }] },
+    { featureType: "poi.park", elementType: "geometry.fill", stylers: [{ color: "#023e58" }] },
+    { featureType: "road", elementType: "geometry", stylers: [{ color: "#304a7d" }] },
+    { featureType: "road", elementType: "labels.text.fill", stylers: [{ color: "#98a5be" }] },
+    { featureType: "road.highway", elementType: "geometry", stylers: [{ color: "#2c6675" }] },
+    { featureType: "transit", elementType: "labels.text.fill", stylers: [{ color: "#98a5be" }] },
+    { featureType: "water", elementType: "geometry", stylers: [{ color: "#0e1626" }] },
+];
+
 function Heatmap() {
     const map = useMap();
     const [heatmap, setHeatmap] = React.useState<google.maps.visualization.HeatmapLayer | null>(null);
 
     React.useEffect(() => {
         if (!map) return;
-
-        const mapStyles: google.maps.MapTypeStyle[] = [
-            { elementType: "geometry", stylers: [{ color: "#1d2c4d" }] },
-            { elementType: "labels.text.fill", stylers: [{ color: "#8ec3b9" }] },
-            { elementType: "labels.text.stroke", stylers: [{ color: "#1a3646" }] },
-            { featureType: "administrative.country", elementType: "geometry.stroke", stylers: [{ color: "#4b6878" }] },
-            { featureType: "administrative.land_parcel", stylers: [{ visibility: "off" }] },
-            { featureType: "administrative.province", elementType: "geometry.stroke", stylers: [{ color: "#4b6878" }] },
-            { featureType: "landscape.man_made", elementType: "geometry.stroke", stylers: [{ color: "#334e87" }] },
-            { featureType: "landscape.natural", elementType: "geometry", stylers: [{ color: "#023e58" }] },
-            { featureType: "poi", elementType: "geometry", stylers: [{ color: "#283d6a" }] },
-            { featureType: "poi", elementType: "labels.text.fill", stylers: [{ color: "#6f9ba5" }] },
-            { featureType: "poi.park", elementType: "geometry.fill", stylers: [{ color: "#023e58" }] },
-            { featureType: "road", elementType: "geometry", stylers: [{ color: "#304a7d" }] },
-            { featureType: "road", elementType: "labels.text.fill", stylers: [{ color: "#98a5be" }] },
-            { featureType: "road.highway", elementType: "geometry", stylers: [{ color: "#2c6675" }] },
-            { featureType: "transit", elementType: "labels.text.fill", stylers: [{ color: "#98a5be" }] },
-            { featureType: "water", elementType: "geometry", stylers: [{ color: "#0e1626" }] },
-        ];
-        
-        map.setOptions({ styles: mapStyles, disableDefaultUI: true });
 
         const newHeatmap = new google.maps.visualization.HeatmapLayer({
             data: generateHeatmapData(),
@@ -210,6 +208,7 @@ export function NetworkMapSection() {
                                         gestureHandling={'greedy'}
                                         disableDefaultUI={true}
                                         mapId="network-heatmap"
+                                        styles={mapStyles}
                                     >
                                         <Heatmap />
                                     </Map>
