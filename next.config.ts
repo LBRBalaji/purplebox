c7cf8fcbc385832c6e222150b70a77ace181121b
Balaji ARAM VALARTHA SUNDARAM <balaji@lakshmibalajio2o.com>
2025-07-29 09:39:44 +0000

    The NextJS app server is having trouble starting. Please identify what c

--- next.config.ts
+++ next.config.ts
@@ -19,10 +19,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  env: {
-    NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
-    NEXT_PUBLIC_MAP_ID: process.env.NEXT_PUBLIC_MAP_ID,
-  }
 };
 
 export default nextConfig;
