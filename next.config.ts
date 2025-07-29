72e60a1b2a098373ffe7bdea8e822891dbdae4c4
Balaji ARAM VALARTHA SUNDARAM <balaji@lakshmibalajio2o.com>
2025-07-29 09:35:26 +0000

    issue still persist: Map Feature Not Available The Google Maps API key o

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,4 @@
+
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
@@ -18,6 +19,10 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  env: {
+    NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
+    NEXT_PUBLIC_MAP_ID: process.env.NEXT_PUBLIC_MAP_ID,
+  }
 };
 
 export default nextConfig;
